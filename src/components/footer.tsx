a53ba4aebea28c6e4ada916f521d417b51d26f29
gabin-ishimwe <g.ishimwe@alustudent.com>
2023-04-24 03:02:23 +0200

    ft: add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="mx-auto max-w-[1920px] px-6 bg-zinc-100">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-zinc-400 py-12 text-black transition-colors duration-150 bg-zinc-100">
+        <div className="col-span-1 lg:col-span-2">
+          <Link
+            className="flex flex-initial items-center font-bold md:mr-24"
+            href="/">
+            <span className="mb-6 text-2xl">Chatbase</span>
+          </Link>
+          <a className="block text-sm text-zinc-600" href="/privacy">
+            Privacy Policy
+          </a>
+          <a className="block text-sm text-zinc-600" href="/terms">
+            Terms of Service
+          </a>
+        </div>
+        <div className="col-span-1 lg:col-span-2"></div>
+        <div className="col-span-1 lg:col-span-2"></div>
+        <div className="col-span-1 lg:col-span-6 flex items-start lg:justify-end text-black">
+          <div className="flex space-x-6 items-center h-10">
+            <a aria-label="Twitter Account" target="_blank" href="#">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                  fill="currentColor"></path>
+              </svg>
+            </a>
+            <a aria-label="LinkedIn Account" target="_blank" href="#">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                  fill="currentColor"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <p className="py-5">
+        Contact:<a href="#"> support@chatbase.co</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,76 @@
-import { Dialog } from '@headlessui/react'
-import { Inter } from 'next/font/google'
-import Image from 'next/image'
-import { useState } from 'react'
+import { Dialog } from "@headlessui/react";
+import { Inter } from "next/font/google";
+import Image from "next/image";
+import { useState } from "react";
 // import { HiBars3,  } from 'react-icons/hi'
-import FaQs from '@/components/FaQs'
-import LiveDemo from '@/components/LiveDemo'
-import VideoDemo from '@/components/videoDemo'
-import { FaBars } from "react-icons/fa"
-import { IoMdClose } from 'react-icons/io'
-import ProductHunt from '../../public/images/producthunt-bot.png'
+import FaQs from "@/components/FaQs";
+import LiveDemo from "@/components/LiveDemo";
+import Testimonials from "@/components/Testimonials";
+import Footer from "@/components/footer";
+import VideoDemo from "@/components/videoDemo";
+import { FaBars } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+import ProductHunt from "../../public/images/producthunt-bot.png";
 
 const navigation = [
-  { name: 'Demo', href: '#' },
-  { name: 'Pricing', href: '#' },
-  { name: 'Featured Bots', href: '#' },
-  { name: 'My ChatBots', href: '#' },
-]
+  { name: "Demo", href: "#" },
+  { name: "Pricing", href: "#" },
+  { name: "Featured Bots", href: "#" },
+  { name: "My ChatBots", href: "#" },
+];
 
-const inter = Inter({ subsets: ['latin'] })
-
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <div className="bg-white isolate">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global">
           <div className="flex items-center space-x-4 lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            {/* <a href="#" className="-m-1.5 p-1.5">
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
               />
-            </a>
-              <span className="text-xl">Chatbase Co</span>
+            </a> */}
+            <span className="text-2xl font-bold">Chatbase Co</span>
           </div>
           <div className="flex lg:hidden">
             <button
               type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
+              onClick={() => setMobileMenuOpen(true)}>
               <span className="sr-only">Open main menu</span>
               <FaBars className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900">
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900">
               Account <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
@@ -75,8 +85,7 @@ export default function Home() {
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
+                onClick={() => setMobileMenuOpen(false)}>
                 <span className="sr-only">Close menu</span>
                 <IoMdClose className="h-6 w-6" aria-hidden="true" />
               </button>
@@ -88,8 +97,7 @@ export default function Home() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                       {item.name}
                     </a>
                   ))}
@@ -97,8 +105,7 @@ export default function Home() {
                 <div className="py-6">
                   <a
                     href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Account
                   </a>
                 </div>
@@ -107,63 +114,67 @@ export default function Home() {
           </Dialog.Panel>
         </Dialog>
       </header>
-      <div className='max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8'>
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
+      <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
+        <div className="relative isolate px-6 pt-14 lg:px-8">
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
-        <div className="relative px-6 lg:px-8 py-16 sm:py-8">
-          <div className='flex items-center justify-between space-x-8'>
-          <div className="text-center w-[38rem]">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Custom ChatGPT for <span className='text-transparent bg-clip-text bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-purple-200 via-violet-700 to-rose-700'>your website</span> 
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-            Just upload a document or add a link to your website and get a ChatGPT-like chatbot that can answer any question on it. Then add a chat widget to your website.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Build your chatbot
-              </a>
-              {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true">
+            <div
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+          <div className="relative px-6 lg:px-8 py-16 sm:py-8">
+            <div className="flex items-center justify-between space-x-8">
+              <div className="text-center w-[38rem]">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  Custom ChatGPT for{" "}
+                  <span className="text-transparent bg-clip-text bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-purple-200 via-violet-700 to-rose-700">
+                    your website
+                  </span>
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Just upload a document or add a link to your website and get a
+                  ChatGPT-like chatbot that can answer any question on it. Then
+                  add a chat widget to your website.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Build your chatbot
+                  </a>
+                  {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
               </a> */}
+                </div>
+              </div>
+              <div className="hidden md:block">
+                <Image src={ProductHunt} alt="product hunt" width={270} />
+              </div>
             </div>
           </div>
-          <div className='hidden md:block'>
-            <Image src={ProductHunt} alt='product hunt' width={270}/>
-          </div>
-          </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true">
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
         </div>
+        <VideoDemo />
+        <LiveDemo />
+        <FaQs />
+        <Testimonials />
       </div>
-      <VideoDemo/>
-      <LiveDemo/>
-      <FaQs/>
+      <Footer />
     </div>
-    </div>
-  )
+  );
 }
